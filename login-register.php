17b4877fa95b1a32e78343b5e58998660017ce6a
alwayswannaflame <radu.damian.badescu@gmail.com>
2018-08-19 21:03:50 +0300

    add viewport meta and jquery

--- login-register.php
+++ login-register.php
@@ -6,6 +6,7 @@
 <html>
 <head>
 	<title>Co-founder - login-register</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
 </head>
@@ -122,6 +123,8 @@
 		</div>
 	</div>
 	<?php include 'templates/footer.php';?>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 </body>
 </html>
